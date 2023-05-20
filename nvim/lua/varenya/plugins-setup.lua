51cd5c6bb3a94fb7894b83483f33feb6d5d6a848
Varenya Thyagaraj <varenya@Varenyas-MacBook-Air.local>
2023-05-20 22:57:53 +0530

    check packer on init

--- nvim/lua/varenya/plugins-setup.lua
+++ nvim/lua/varenya/plugins-setup.lua
@@ -1,7 +1,34 @@
-vim.cmd [[packadd packer.nvim]]
+local ensure_packer = function()
+	local fn = vim.fn
+	local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+		vim.cmd [[packadd packer.nvim]]
+		return true
+	end
+	return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+-- autocommand that reloads neovim and installs/updates/removes plugins
+-- when file is saved
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins-setup.lua source <afile> | PackerSync
+  augroup end
+]])
+
+-- import packer safely
+local status, packer = pcall(require, "packer")
+if not status then
+	return
+end
 
 
-return require('packer').startup(function(use)
+
+return packer.startup(function(use)
 	use 'wbthomason/packer.nvim'
 	use 'bluz71/vim-nightfly-colors'
 	use "EdenEast/nightfox.nvim"
@@ -43,7 +70,12 @@ return require('packer').startup(function(use)
 	use('jose-elias-alvarez/typescript.nvim');
 	use('jose-elias-alvarez/null-ls.nvim')
 	use('MunifTanjim/prettier.nvim');
-	use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' });
+	use { 'nvim-treesitter/nvim-treesitter',
+		run = function()
+			local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+			ts_update()
+		end,
+	};
 	use("windwp/nvim-ts-autotag");
 	use('windwp/nvim-autopairs');
 	use('folke/tokyonight.nvim');
